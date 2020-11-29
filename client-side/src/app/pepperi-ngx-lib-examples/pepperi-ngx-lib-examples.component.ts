dbe6d7b1ecbebc842bcb06aed2d9646cdf3bd628
meital-pepperi <meital.b@pepperi.com>
2020-11-29 16:06:29 +0200

    update ngx-lib

--- client-side/src/app/pepperi-ngx-lib-examples/pepperi-ngx-lib-examples.component.ts
+++ client-side/src/app/pepperi-ngx-lib-examples/pepperi-ngx-lib-examples.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PepFieldClickedData, PepFieldValueChangedData } from '@pepperi-addons/ngx-lib';
-import { PepGroupButtonsViewType, PepGroupButton } from '@pepperi-addons/ngx-lib/group-buttons';
+import { PepGroupButtonsViewType } from '@pepperi-addons/ngx-lib/group-buttons';
 
 @Component({
     // tslint:disable-next-line: component-selector
@@ -13,17 +13,11 @@ export class PepperiNgxLibExamplesComponent implements OnInit {
     title = 'client-side';
     minDateValue: number;
     maxDateValue: number;
-    groupButtons: Array<PepGroupButton>;
     richHtml;
     
     constructor() { 
         this.minDateValue = new Date(1-1-2019).getTime();
         this.maxDateValue = new Date(1-1-2021).getTime();
-        
-        this.groupButtons = [
-            { key: '', value: 'test', class: '', callback: (event) => this.onGroupButtonClicked(event, 'test'), icon: null },
-            { key: '', value: '', class: 'caution', callback: (event) => this.onGroupButtonClicked(event, 'del'), icon: 'system_bin' },
-        ];
         
         this.richHtml = "<h1><u>Rich Text Value Example</u></h1><h2><em style=' color: rgb(147, 200, 14);'>Pepperi Rich Text Value </em><u style='color: rgb(0, 102, 204);'>Example</u></h2><ol><li><strong><u>Pepperi Rich Text Value Example</u></strong></li><li>Pepperi Rich text [value] example</li></ol>";
     }
@@ -42,9 +36,5 @@ export class PepperiNgxLibExamplesComponent implements OnInit {
     
     onMenuItemClicked(event) {
         alert(`${event.key}: was clicked`);
-    }
-
-    onGroupButtonClicked(event: PepGroupButton, title): void {
-        alert(title);
     }
 }
